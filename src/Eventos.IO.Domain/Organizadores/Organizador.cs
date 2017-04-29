9146d719d5938c23a9aa7c79ed60144784c340da
Elton Garbin <eltongarbin@hotmail.com>
2017-04-29 18:25:49 -0300

    Complementando entidade organizador

--- src/Eventos.IO.Domain/Organizadores/Organizador.cs
+++ src/Eventos.IO.Domain/Organizadores/Organizador.cs
@@ -1,18 +1,31 @@
-﻿using System;
-using Eventos.IO.Domain.Core.Models;
+﻿using Eventos.IO.Domain.Core.Models;
+using Eventos.IO.Domain.Eventos;
+using System;
+using System.Collections.Generic;
 
 namespace Eventos.IO.Domain.Organizadores
 {
     public class Organizador : Entity<Organizador>
     {
-        public Organizador(Guid id)
+        public string Nome { get; private set; }
+        public string CPF { get; private set; }
+        public string Email { get; private set; }
+
+        public virtual ICollection<Evento> Eventos { get; set; }
+
+        public Organizador(Guid id, string nome, string cpf, string email)
         {
             Id = id;
+            Nome = nome;
+            CPF = cpf;
+            Email = email;
         }
+
+        public Organizador() { }
 
         public override bool EhValido()
         {
-            throw new NotImplementedException();
+            return true;
         }
     }
 }
