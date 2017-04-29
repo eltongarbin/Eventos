5841ecc9c9a13266b0593323705e220c3961c9f7
Elton Garbin <eltongarbin@hotmail.com>
2017-04-29 18:28:36 -0300

    Infra criada e projeto web criado apenas para rodar o update-database do ef

---
+++ src/Eventos.IO.Infra.Data/Mappings/EnderecoMapping.cs
@@ -0,0 +1,58 @@
+﻿using Eventos.IO.Domain.Eventos;
+using Eventos.IO.Infra.Data.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Eventos.IO.Infra.Data.Mappings
+{
+    public class EnderecoMapping : EntityTypeConfiguration<Endereco>
+    {
+        public override void Map(EntityTypeBuilder<Endereco> builder)
+        {
+            builder.Property(e => e.Logradouro)
+                .HasColumnType("varchar(150)")
+                .HasMaxLength(150)
+                .IsRequired();
+
+            builder.Property(e => e.Numero)
+                .HasColumnType("varchar(20)")
+                .HasMaxLength(20)
+                .IsRequired();
+
+            builder.Property(e => e.Bairro)
+                .HasColumnType("varchar(50)")
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.Property(e => e.CEP)
+                .HasColumnType("varchar(8)")
+                .HasMaxLength(8)
+                .IsRequired();
+
+            builder.Property(e => e.Complemento)
+                .HasColumnType("varchar(100)")
+                .HasMaxLength(100);
+
+            builder.Property(e => e.Cidade)
+                .HasColumnType("varchar(100)")
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder.Property(e => e.Estado)
+                .HasColumnType("varchar(100)")
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder.HasOne(c => c.Evento)
+                .WithOne(c => c.Endereco)
+                .HasForeignKey<Endereco>(c => c.EventoId)
+                .IsRequired(false);
+
+            builder.Ignore(c => c.ValidationResult);
+
+            builder.Ignore(c => c.CascadeMode);
+
+            builder.ToTable("Enderecos");
+        }
+    }
+}
