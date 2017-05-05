19de30d1cd6641902184a547ce745f7845cace26
Elton Garbin <eltongarbin@hotmail.com>
2017-05-04 22:48:32 -0300

    Adicionando projetos Ioc e Bus na camada de CrossCutting

---
+++ src/Eventos.IO.Infra.CrossCutting.IoC/NativeInjectorBootStrapper.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using Eventos.IO.Application.Interfaces;
+using Eventos.IO.Application.Services;
+using Eventos.IO.Domain.Core.Bus;
+using Eventos.IO.Domain.Core.Events;
+using Eventos.IO.Domain.Core.Notifications;
+using Eventos.IO.Domain.Eventos.Commands;
+using Eventos.IO.Domain.Eventos.EventHandlers;
+using Eventos.IO.Domain.Eventos.Events;
+using Eventos.IO.Domain.Eventos.Repository;
+using Eventos.IO.Domain.Interfaces;
+using Eventos.IO.Infra.CrossCutting.Bus;
+using Eventos.IO.Infra.Data.Context;
+using Eventos.IO.Infra.Data.Repository;
+using Eventos.IO.Infra.Data.UoW;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Eventos.IO.Infra.CrossCutting.IoC
+{
+    public class NativeInjectorBootStrapper
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            #region Application
+            services.AddSingleton(Mapper.Configuration);
+            services.AddScoped<IMapper>(sp => new Mapper(sp.GetRequiredService<IConfigurationProvider>(), sp.GetService));
+            services.AddScoped<IEventoAppService, EventoAppService>();
+            #endregion
+
+            #region Domain
+            // Commands
+            services.AddScoped<IHandler<RegistrarEventoCommand>, EventoCommandHandler>();
+            services.AddScoped<IHandler<AtualizarEventoCommand>, EventoCommandHandler>();
+            services.AddScoped<IHandler<ExcluirEventoCommand>, EventoCommandHandler>();
+
+            // Eventos
+            services.AddScoped<IDomainNotificationHandler<DomainNotification>, DomainNotificationHandler>();
+            services.AddScoped<IHandler<EventoRegistradoEvent>, EventoEventHandler>();
+            services.AddScoped<IHandler<EventoAtualizadoEvent>, EventoEventHandler>();
+            services.AddScoped<IHandler<EventoExcluidoEvent>, EventoEventHandler>();
+            #endregion
+
+            #region Infra
+            // Data
+            services.AddScoped<IEventoRepository, EventoRepository>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<EventosContext>();
+
+            // Bus
+            services.AddScoped<IBus, InMemoryBus>();
+            #endregion
+        }
+    }
+}
