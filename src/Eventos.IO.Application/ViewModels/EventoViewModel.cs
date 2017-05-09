2f7e8dd82b4c4350e6744d8b7d61fb3625c373fb
Elton Garbin <eltongarbin@hotmail.com>
2017-05-08 21:48:41 -0300

    Ajustando DataAnnotations

--- src/Eventos.IO.Application/ViewModels/EventoViewModel.cs
+++ src/Eventos.IO.Application/ViewModels/EventoViewModel.cs
@@ -22,13 +22,11 @@ namespace Eventos.IO.Application.ViewModels
         public string DescricaoLonga { get; set; }
 
         [Display(Name = "Início do Evento")]
-        [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        [Required(ErrorMessage = "A data é requerida")]
         public DateTime DataInicio { get; set; }
 
         [Display(Name = "Fim do Evento")]
-        [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        [Required(ErrorMessage = "A data é requerida")]
         public DateTime DataFim { get; set; }
 
         [Display(Name = "Será gratuito?")]
@@ -36,6 +34,7 @@ namespace Eventos.IO.Application.ViewModels
 
         [Display(Name = "Valor")]
         [DisplayFormat(DataFormatString = "{0:C}")]
+        [DataType(DataType.Currency, ErrorMessage = "Moeda em formato inválido")]
         public decimal Valor { get; set; }
 
         [Display(Name = "Será online?")]
