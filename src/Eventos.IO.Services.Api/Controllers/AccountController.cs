a64b49117852a12368aa41dbd2b7d6efee4508a5
Elton Garbin <eltongarbin@hotmail.com>
2017-05-17 23:56:48 -0300

    Criando camada de serviços rest

---
+++ src/Eventos.IO.Services.Api/Controllers/AccountController.cs
@@ -0,0 +1,105 @@
+using Eventos.IO.Domain.Core.Bus;
+using Eventos.IO.Domain.Core.Notifications;
+using Eventos.IO.Domain.Interfaces;
+using Eventos.IO.Domain.Organizadores.Commands;
+using Eventos.IO.Infra.CrossCutting.Identity.Models;
+using Eventos.IO.Infra.CrossCutting.Identity.Models.AccountViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace Eventos.IO.Services.Api.Controllers
+{
+    public class AccountController : BaseController
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly ILogger _logger;
+        private readonly IBus _bus;
+
+        public AccountController(IDomainNotificationHandler<DomainNotification> notifications,
+                                 IUser user,
+                                 UserManager<ApplicationUser> userManager,
+                                 SignInManager<ApplicationUser> signInManager,
+                                 ILoggerFactory loggerFactory,
+                                 IBus bus)
+            : base(notifications, user, bus)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _bus = bus;
+
+            _logger = loggerFactory.CreateLogger<AccountController>();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [Route("nova-conta")]
+        public async Task<IActionResult> Register([FromBody] RegisterViewModel model, int version)
+        {
+            if (version == 2)
+            {
+                return Response(new
+                {
+                    Message = "API V2 não disponível"
+                });
+            }
+
+            if (!ModelState.IsValid)
+            {
+                NotificarErroModelInvalida();
+                return Response(model);
+            }
+
+            var user = new ApplicationUser
+            {
+                UserName = model.Email,
+                Email = model.Email
+            };
+
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                var registroCommand = new RegistrarOrganizadorCommand(Guid.Parse(user.Id), model.Nome, model.CPF, user.Email);
+                _bus.SendCommand(registroCommand);
+
+                if (!OperacaoValida())
+                {
+                    await _userManager.DeleteAsync(user);
+                    return Response(model);
+                }
+
+                _logger.LogInformation(1, "Usuário criado com sucesso!");
+                return Response(model);
+            }
+
+            AdicionarErrosIdentity(result);
+            return Response(model);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [Route("conta")]
+        public async Task<IActionResult> Login([FromBody] LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                NotificarErroModelInvalida();
+                return Response(model);
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, false, true);
+            if (result.Succeeded)
+            {
+                _logger.LogInformation(1, "Usuário logado com sucesso");
+                return Response(model);
+            }
+
+            NotificarErro(result.ToString(), "Falha ao realizar o login");
+            return Response(model);
+        }
+    }
+}
---
+++ src/Eventos.IO.Services.Api/Startup.cs
@@ -0,0 +1,74 @@
+﻿using AutoMapper;
+using Eventos.IO.Infra.CrossCutting.Bus;
+using Eventos.IO.Infra.CrossCutting.Identity.Data;
+using Eventos.IO.Infra.CrossCutting.Identity.Models;
+using Eventos.IO.Infra.CrossCutting.IoC;
+using Eventos.IO.Services.Api.Configurations;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Eventos.IO.Services.Api
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", false, true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddOptions();
+            services.AddMvc(options =>
+            {
+                options.OutputFormatters.Remove(new XmlDataContractSerializerOutputFormatter());
+                options.UseCentralRoutePrefix(new RouteAttribute("api/v{version}"));
+            });
+            services.AddAutoMapper();
+
+            RegisterServices(services);
+        }
+
+        public void Configure(IApplicationBuilder app, 
+                              IHostingEnvironment env, 
+                              ILoggerFactory loggerFactory,
+                              IHttpContextAccessor accessor)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            app.UseStaticFiles();
+            app.UseIdentity();
+            app.UseMvc();
+
+            InMemoryBus.ContainerAccessor = () => accessor.HttpContext.RequestServices;
+        }
+
+        public static void RegisterServices(IServiceCollection services)
+        {
+            NativeInjectorBootStrapper.RegisterServices(services);
+        }
+    }
+}
