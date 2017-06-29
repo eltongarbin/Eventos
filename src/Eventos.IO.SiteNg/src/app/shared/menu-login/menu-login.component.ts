f4601315ac2f9330b8a2dbeef626b5413fd562e2
Elton Garbin <eltongarbin@hotmail.com>
2017-06-28 23:27:22 -0300

    Status do usuário

---
+++ src/Eventos.IO.SiteNg/src/app/shared/menu-login/menu-login.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+
+@Component({
+    selector: 'app-menu-login',
+    templateUrl: './menu-login.component.html'
+})
+export class MenuLoginComponent implements OnInit {
+    public token;
+    public user;
+    public nome: string = "";
+
+    constructor(private router: Router) {
+        this.token = localStorage.getItem('eio.token');
+        this.user = JSON.parse(localStorage.getItem('eio.user'));
+    }
+
+    usuarioLogado(): boolean {
+        return this.token !== null;
+    }
+
+    logout() {
+        localStorage.removeItem('eio.token');
+        localStorage.removeItem('eio.user');
+        this.router.navigateByUrl('./home');
+    }
+
+    ngOnInit() {
+        if (this.user) {
+            this.nome = this.user.userName;
+        }
+    }
+}
