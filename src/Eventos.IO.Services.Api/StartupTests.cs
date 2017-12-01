8cc6c7da4d90f531e148bbf896d0da6d02665646
Elton Diego Garbin do Nascimento <eltongarbin@gmail.com>
2017-12-01 21:15:40 -0200

    Configurando testes de integração.

---
+++ src/Eventos.IO.Services.Api/StartupTests.cs
@@ -0,0 +1,162 @@
+﻿using AutoMapper;
+using Elmah.Io.AspNetCore;
+using Elmah.Io.Extensions.Logging;
+using Eventos.IO.Infra.CrossCutting.AspNetFilters;
+using Eventos.IO.Infra.CrossCutting.Identity.Authorization;
+using Eventos.IO.Infra.CrossCutting.Identity.Data;
+using Eventos.IO.Infra.CrossCutting.Identity.Models;
+using Eventos.IO.Infra.CrossCutting.IoC;
+using Eventos.IO.Services.Api.Configurations;
+using Eventos.IO.Services.Api.Middlewares;
+using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using Swashbuckle.AspNetCore.Swagger;
+using System;
+using System.Text;
+using IConfigurationProvider = AutoMapper.IConfigurationProvider;
+
+namespace Eventos.IO.Services.Api
+{
+    public class StartupTests
+    {
+        private const string SecretKey = "eventosio@meuambienteToken";
+        private readonly SymmetricSecurityKey _signingKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(SecretKey));
+
+        public IConfigurationRoot Configuration { get; }
+
+        public StartupTests(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            //services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+            //{
+            //    options.Cookies.ApplicationCookie.AutomaticChallenge = false;
+            //})
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddOptions();
+
+            services.AddMvc(options =>
+            {
+                options.OutputFormatters.Remove(new XmlDataContractSerializerOutputFormatter());
+                options.UseCentralRoutePrefix(new RouteAttribute("api/v{version}"));
+
+                var policy = new AuthorizationPolicyBuilder()
+                    .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
+                    .RequireAuthenticatedUser()
+                    .Build();
+
+                options.Filters.Add(new AuthorizeFilter(policy));
+                options.Filters.Add(new ServiceFilterAttribute(typeof(GlobalActionLogger)));
+            });
+
+            var jwtAppSettingOptions = Configuration.GetSection(nameof(JwtTokenOptions));
+            services.Configure<JwtTokenOptions>(options =>
+            {
+                options.Issuer = jwtAppSettingOptions[nameof(JwtTokenOptions.Issuer)];
+                options.Audience = jwtAppSettingOptions[nameof(JwtTokenOptions.Audience)];
+                options.SigningCredentials = new SigningCredentials(_signingKey, SecurityAlgorithms.HmacSha256);
+            });
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(jwt =>
+                {
+                    jwt.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidIssuer = jwtAppSettingOptions[nameof(JwtTokenOptions.Issuer)],
+
+                        ValidateAudience = true,
+                        ValidAudience = jwtAppSettingOptions[nameof(JwtTokenOptions.Audience)],
+
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = _signingKey,
+
+                        RequireExpirationTime = true,
+                        ValidateLifetime = true,
+
+                        ClockSkew = TimeSpan.Zero
+                    };
+                });
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("PodeLerEventos", policy => policy.RequireClaim("Eventos", "Ler"));
+                options.AddPolicy("PodeGravar", policy => policy.RequireClaim("Eventos", "Gravar"));
+            });
+
+            services.AddAutoMapper();
+
+            services.AddSingleton(Mapper.Configuration);
+            services.AddScoped<IMapper>(sp => new Mapper(sp.GetRequiredService<IConfigurationProvider>(), sp.GetService));
+
+            services.AddMediatR(typeof(Startup));
+
+            RegisterServices(services);
+        }
+
+        public void Configure(IApplicationBuilder app,
+                              IHostingEnvironment env,
+                              ILoggerFactory loggerFactory,
+                              IHttpContextAccessor accessor)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+            loggerFactory.AddElmahIo("31737484568c41429cccb10414b416fd", new Guid("357211f6-c783-4562-87ab-dec2a873958c"));
+
+            var elmahSts = new ElmahIoSettings
+            {
+                OnMessage = message =>
+                {
+                    message.Version = "v1.0";
+                    message.Application = "Eventos.IO";
+                    message.User = accessor.HttpContext.User.Identity.Name;
+                }
+            };
+
+            app.UseElmahIo("31737484568c41429cccb10414b416fd", new Guid("357211f6-c783-4562-87ab-dec2a873958c"), elmahSts);
+
+            app.UseCors(c =>
+            {
+                c.AllowAnyHeader();
+                c.AllowAnyMethod();
+                c.AllowAnyOrigin();
+            });
+
+            app.UseStaticFiles();
+            app.UseAuthentication();
+            app.UseMvc();
+        }
+
+        private static void RegisterServices(IServiceCollection services)
+        {
+            NativeInjectorBootStrapper.RegisterServices(services);
+        }
+    }
+}
---
+++ tests/Eventos.IO.Tests.Api/IntegrationTests/Environment.cs
@@ -0,0 +1,25 @@
+﻿using Eventos.IO.Services.Api;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using System.Net.Http;
+
+namespace Eventos.IO.Tests.Api.IntegrationTests
+{
+    public class Environment
+    {
+        public static TestServer Server;
+        public static HttpClient Client;
+
+        public static void CriarServidor()
+        {
+            var webHostBuilder = new WebHostBuilder()
+                .UseEnvironment("Testing")
+                .UseUrls("http://localhost:8285")
+                .UseStartup<StartupTests>();
+
+            Server = new TestServer(webHostBuilder);
+
+            Client = Server.CreateClient();
+        }
+    }
+}
