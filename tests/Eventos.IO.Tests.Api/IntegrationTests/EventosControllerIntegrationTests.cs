51806cd488d076114d7b5979aea413fd6bee8a27
Elton Diego Garbin do Nascimento <eltongarbin@gmail.com>
2017-12-02 14:41:09 -0200

    Adicionando novos testes de integração

---
+++ tests/Eventos.IO.Tests.Api/IntegrationTests/EventosControllerIntegrationTests.cs
@@ -0,0 +1,89 @@
+﻿using Eventos.IO.Services.Api.ViewModels;
+using Eventos.IO.Tests.Api.IntegrationTests.DTO;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Eventos.IO.Tests.Api.IntegrationTests
+{
+    public class EventosControllerIntegrationTests
+    {
+        public EventosControllerIntegrationTests()
+        {
+            Environment.CriarServidor();
+        }
+
+        [Fact]
+        public async Task EventosController_ObterListaEventos_RetornarJsonComSucesso()
+        {
+            // Arrange & Act
+            var response = await Environment.Client.GetAsync("api/v1/eventos");
+            var responseEvento = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.NotEmpty(responseEvento);
+        }
+
+        [Fact]
+        public async Task EventosController_ObterListaMeusEventos_RetornarJsonComSucesso()
+        {
+            // Arrange
+            var user = await UserUtils.RealizarLoginOrganizador(Environment.Client);
+
+            // Act
+            var response = await Environment.Server
+                .CreateRequest("api/v1/eventos/meus-eventos")
+                .AddHeader("Content-Type", "application/json")
+                .AddHeader("Authorization", string.Concat("Bearer ", user.access_token))
+                .GetAsync();
+
+            var responseEvento = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.NotEmpty(responseEvento);
+        }
+
+        [Fact]
+        public async Task EventosController_RegistrarNovoEvento_RetornarComSucesso()
+        {
+            // Arrange
+            var login = await UserUtils.RealizarLoginOrganizador(Environment.Client);
+            var evento = new EventoViewModel
+            {
+                Nome = "DevXperience",
+                DescricaoCurta = "Um evento de tecnologia",
+                DescricaoLonga = "Um evento de tecnologia",
+                CategoriaId = new Guid("ac381ba8-c187-42c-a5cb-899ad7176137"),
+                DataInicio = DateTime.Now.AddDays(1),
+                DataFim = DateTime.Now.AddDays(2),
+                Gratuito = false,
+                Valor = 500,
+                NomeEmpresa = "DevX",
+                Online = true,
+                Endereco = new EnderecoViewModel(),
+                OrganizadorId = new Guid(login.user.id)
+            };
+
+            // Act
+            var response = await Environment.Server
+                .CreateRequest("api/v1/eventos")
+                .AddHeader("Authorization", string.Concat("Bearer ", login.access_token))
+                .And(request => request.Content = new StringContent(JsonConvert.SerializeObject(evento), 
+                                                                    Encoding.UTF8,
+                                                                    "application/json"))
+                // .And(request => request.Method = HttpMethod.Put)
+                .PostAsync();
+
+            var eventoResult = JsonConvert.DeserializeObject<EventoJsonDTO>(await response.Content.ReadAsStringAsync());
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.IsType<EventoDTO>(eventoResult.data);
+        }
+    }
+}
---
+++ tests/Eventos.IO.Tests.Api/IntegrationTests/UserUtils.cs
@@ -0,0 +1,29 @@
+﻿using Eventos.IO.Infra.CrossCutting.Identity.Models.AccountViewModels;
+using Eventos.IO.Tests.Api.IntegrationTests.DTO;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventos.IO.Tests.Api.IntegrationTests
+{
+    public class UserUtils
+    {
+        public static async Task<Result> RealizarLoginOrganizador(HttpClient client)
+        {
+            var user = new LoginViewModel
+            {
+                Email = "elton@gmail.com",
+                Password = "Teste@123"
+            };
+
+            var postContent = new StringContent(JsonConvert.SerializeObject(user), Encoding.UTF8, "application/json");
+            var response = await client.PostAsync("api/v1/conta", postContent);
+
+            var postResult = await response.Content.ReadAsStringAsync();
+            var userResult = JsonConvert.DeserializeObject<UsuarioJsonDTO>(postResult);
+
+            return userResult.data.result;
+        }
+    }
+}
