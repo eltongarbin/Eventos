e5cd99788d55de64a6a925f6e6951441378a5e81
Elton Diego Garbin do Nascimento <eltongarbin@gmail.com>
2017-12-07 20:56:07 -0200

    Preparando para testes automatizados com selenium

---
+++ tests/Eventos.IO.TestesAutomatizados/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Eventos.IO.TestesAutomatizados")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Eventos.IO.TestesAutomatizados")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3bdc9dfc-eea4-421e-bd30-2c91258d0a40")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
